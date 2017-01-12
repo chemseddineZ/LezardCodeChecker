2febe00d5b648b50a3e75a9d952bfcd1da0da1ce
chemseddineZ <chemseddindez@gmail.com>
2017-01-12 02:04:21 +0100

    Final main

--- main/Main.py
+++ main/Main.py
@@ -1,18 +1,12 @@
 import sys
 import ast
 from PyQt4 import QtGui
-#from ui.Frame import Window
-from traitement.Traitement import CountFunc
-#from ui.Frame import CountFunc
+from ui.Frame import Window
 from ui import Frame
 
-class App(QtGui.QApplication):
-    def __init__(self, sys_argv):
-        super(App, self).__init__(sys_argv)
-        self.fc = Frame.CountFunc().GoToPath()
-        self.frame = Frame.Window()
 
 if __name__ == '__main__':
-    app = App(sys.argv)
-    counter = CountFunc()
+    app = QtGui.QApplication(sys.argv)
+    frame = Window()
+    frame.show()
     sys.exit(app.exec_())
